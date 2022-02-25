03ae4b6cffc889175dc3205482bb6e8bdd569afe
Zihad550 <hussainmuzahid83@gmail.com>
2022-02-25 17:52:30 +0600

    practice file updated

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 /*
  * title: Practice node.js
+ * description: simple node application that print random quotes per second interval
  * author: Jehad Hossain
  * date: 25 feb 2022
  */
+
 // dependencies
+const mathLibrary = require('./lib/math');
+const quotesLibrary = require('./lib/quotes');
+
 // app object - module scaffolding
 const app = {};
 
@@ -11,3 +16,30 @@ const app = {};
 app.config = {
     timeBetweenQuotes: 1000,
 };
+
+// function that prints a random quote
+app.printAQuote = function printAQuote() {
+    // get all the quotes
+    const allQuotes = quotesLibrary.allQuotes();
+
+    // get the length of the quotes
+    const numberOfQuotes = allQuotes.length;
+
+    // pick a random number between 1 and the number of quotes
+    const randomNumber = mathLibrary.getRandomNum(1, numberOfQuotes);
+
+    // get the quote at that position in the array (minus one)
+    const selectedQuote = allQuotes[randomNumber - 1];
+
+    // print the quote to the console
+    console.log(selectedQuote);
+};
+
+// function that loops indefinitely, calling the print a quote function as it goes
+app.indefiniteLoop = function indefiniteLoop() {
+    // create the interval, using the config variable defined above
+    setInterval(app.printAQuote, app.config.timeBetweenQuotes);
+};
+
+// invoke the loop
+app.indefiniteLoop();
